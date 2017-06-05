370c78b04a8756ca0178ae70057f4ab0ce65974b
Tommy Barnes <thbarnes@microsoft.com>
2017-06-05 16:11:59 -0700

    first push

---
+++ Desktop/sabre-test/app.js
@@ -0,0 +1,25 @@
+process.env.MICROSOFT_APP_ID = 'cecc3631-a6ed-43a7-9da6-5e60d4063dce';
+process.env.MICROSOFT_APP_PASSWORD = 'anx0UogVrhmDcOmh9n4Lep7';
+
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
